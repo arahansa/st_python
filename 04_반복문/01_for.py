9fa4280092f92c8094148b6070198e0a0a655e0a
arahansa <arahansa@naver.com>
2017-01-14 12:53:50 +0900

    first commi

---
+++ 04_반복문/01_for.py
@@ -0,0 +1,4 @@
+for i in [0, 1, 2, 3, 4]:
+        print(i)
+
+print(list(range(1, 10)))
---
+++ 04_반복문/02_for와리스트.py
@@ -0,0 +1,9 @@
+interest_stocks = ["Naver", "Samsung", "SK Hynix"]
+
+for company in interest_stocks:
+        print("%s: Buy 10" % company)
+
+print("=== 튜플 ===")
+interest_stocks = ("Naver", "Samsung", "SK Hynix")
+for company in interest_stocks:
+    print("%s: Buy 10" % company)
